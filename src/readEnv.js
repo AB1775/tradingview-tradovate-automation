03637696d451b5a519c0a31bc4125daec4f444f5
AB1775 <66264218+AB1775@users.noreply.github.com>
2026-06-22 12:31:16 -0500

    Implement readEnv function to load environment variables

---
+++ src/readEnv.js
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import path from 'path'
+import { credentials, apiURLs } from './Sources/Tradovate/apiHandler.js'
+
+export function readEnv(filePath, func) {
+    try {
+        const resolvedPath = path.resolve(filePath)
+        const envData = fs.readFileSync(resolvedPath, 'utf-8')
+
+        envData.split(/\r?\n/).forEach((line) => {
+            const trimmedLine = line.trim();
+
+            switch(func) {
+                case 0:
+                    try {
+                        if (!trimmedLine || !trimmedLine.includes('https://')) return;
+
+                        const [key, value] = trimmedLine.split('=')
+                        if (key && value !== undefined) {
+                            process.env[key.trim()] = value.trim()
+
+                            apiURLs[key.trim()] = value.trim()
+                        } else {
+                            console.warn('[Credentials Error] Undefined Line in .env File:', line)
+                        }
+                    } catch (e) {
+                        console.error('[API URL Error] Unable to Parse URLs: ', e)
+                    }
+                    break;
+                
+                case 1:
+                    try {
+                        if (!trimmedLine || trimmedLine.startsWith('#')) return;
+                        if (trimmedLine.includes('https://') || trimmedLine.includes('/')) return;
+
+                        const [key, value] = trimmedLine.split('=')
+                        if (key && value !== undefined) {
+                            process.env[key.trim()] = value.trim()
+
+                            credentials[key.trim()] = value.trim()
+                        } else {
+                            console.warn('[Credentials Error] Undefined Line in .env File:', line)
+                        }
+                    } catch (e) {
+                        console.error('[Credentials Error] Unable to Parse Credentials:', e)
+                    }
+                    break;
+                
+                default:
+                    console.error('[readEnv Error] Unrecognized Argument Passed to readEnv:', func)
+            }
+        })
+    } catch (e) {
+        console.error('[readEnv Error] Unable to Process .env File. Check your File Path: ', filePath)
+    }
+}
